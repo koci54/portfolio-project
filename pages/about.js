20edb25ba408e049ce5c8dec2afc84c5543be4c6
koci54 <zen.petr@gmail.com>
2020-04-06 20:15:31 +0200

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Layout from '../components/MyLayout.js'
+
+export default function About() {
+  return (
+    <Layout>
+        <h1>
+        About the project
+        </h1>
+      <p>This is my personal project where I will try to build a webpage step by step.</p>
+        <p>The main goal is to experiment with technologies and learn new skills along the way.</p>
+    </Layout>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Layout from '../components/MyLayout'
+
+
+export default function Blog() {
+    return (
+      <Layout>
+        <h1>Welcome</h1>
+        <p>My name is Petr Kocian.</p>
+        <p>Stay tuned for more.</p>
+      </Layout>
+    );
+  }

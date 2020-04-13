3e0b72325eb824377776daf24f24c1e10ffcdd66
koci54 <zen.petr@gmail.com>
2020-04-13 14:38:50 +0200

    correction commit

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import Layout from '../components/MyLayout.js'
+
+export default function Contact() {
+  return (
+    <Layout>
+        <h1>
+        Contact me
+        </h1>
+      <p>Feel free to reach out to me.</p>
+        <p>petr.kocian@yahoo.com</p>
+    </Layout>
+  )
+}

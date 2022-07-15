6914808670c83a422af17f0f84b21f7f5f8d3bf8
Petr Kocian <zen.petr@gmail.com>
2022-07-15 11:30:53 +0200

    about page update

--- pages/about.js
+++ pages/about.js
@@ -7,8 +7,7 @@ const About = () =>
         <h1>
         About the project
         </h1>
-         <p>This is my personal project where I will try to build a webpage step by step.</p>
-         <p>The main goal is to experiment with technologies and learn new skills along the way.</p>
+          <p>The main goal is to experiment with technologies and learn new skills along the way.</p>
         </div>
     </Layout>
   )

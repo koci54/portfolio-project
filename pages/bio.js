d5a55967125a405d038e7f93ac720b558eff6654
Petr Kocian <zen.petr@gmail.com>
2022-07-17 21:49:12 +0200

    text correction

--- pages/bio.js
+++ pages/bio.js
@@ -7,7 +7,7 @@ const About = () =>
         <h1>
         About me
         </h1>
-          <p>My professional journey in IT started in 2016. I started with web development. By trial and error, I learned how to troubleshoot and use online information when needed. I am grateful for the internet and all its tools that enabled me to work as a cloud system administrator. Some of the tools I use are Python, Shell, Docker, Kubernetes and it is good to know how to call an API.</p>
+          <p>My professional journey in IT began in 2016. I started with web development. By trial and error, I learned how to troubleshoot and use online information when needed. I am grateful for the internet and all its tools that enabled me to work as a cloud system administrator. Some of the tools I use are Python, Shell, Docker, Kubernetes and it is good to know how to call an API.</p>
           <p>I have always been curious about technology and I am trying to learn something new every day. I also speak Czech, English, French, and a little Spanish.</p>
         </div>
     </Layout>
